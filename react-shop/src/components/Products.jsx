4791c242392eef70b104c83a22b4707c4c70cbb9
heopoint <36278561+heopoint@users.noreply.github.com>
2024-05-19 19:51:07 +0900

    이미지 추가

--- react-shop/src/components/Products.jsx
+++ react-shop/src/components/Products.jsx
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export default function Products({ item }) {
   return (
     <Col>
-         <img src={`../assets/shoes${item.id}.jpg`} alt={item.title} />
+         <img src={`https://heopoint.github.io/react_work/react-shop/src/assets/shoes${item.id}.jpg`} alt={item.title} />
       <h4>{item.title}</h4>
      <h4>{item.content}</h4>
       <p>{item.price}</p>
